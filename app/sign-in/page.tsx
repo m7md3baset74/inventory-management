97ad71fce2d438fcecd423d3eaf68f97bafbe22e
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2025-10-14 03:34:46 +0300

    first commit

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,7 +10,7 @@ export default async function SignInPage() {
   }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-8 px-5 md:px-0">
         <SignIn />
         <Link href="/"> Go Back Home</Link>
       </div>
